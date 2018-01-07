fc6e5fdf9b5264f60a9acc440d45e545d0008de4
ampersand8 <ampersand8@gmail.com>
2018-01-07 21:23:29 +0100

    setup basic login/register/logout

---
+++ src/main/java/com/swisscom/beans/UserBean.java
@@ -0,0 +1,105 @@
+package com.swisscom.beans;
+
+import com.swisscom.model.User;
+import com.swisscom.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@ManagedBean(name = "userBean")
+@SessionScoped
+public class UserBean {
+    private String username;
+    private String password;
+    private boolean anonymous = true;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getAnonymous() {
+        return this.anonymous;
+    }
+
+    public String add() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        User user = new User(username, password);
+        try {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            this.anonymous = false;
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return user.getId();
+    }
+
+    public String login() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> query = builder.createQuery(User.class);
+            Root<User> root = query.from(User.class);
+            query.select(root).where(builder.equal(root.get("username"), username));
+            Query<User> q = session.createQuery(query);
+            User user = q.getSingleResult();
+            transaction.commit();
+            if (password.equals(user.getPassword())) {
+                this.anonymous = false;
+                return "output";
+            } else {
+                return "invalid";
+            }
+
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return "invalid";
+    }
+
+    public void logout() {
+        try {
+            this.anonymous = true;
+            FacesContext context = FacesContext.getCurrentInstance();
+            ExternalContext ec = context.getExternalContext();
+            ec.redirect("index.xhtml");
+            final HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+            request.getSession(false).invalidate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
