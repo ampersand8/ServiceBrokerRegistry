a125bb6351c1bf723e297270c05eba99ab282dc2
ampersand8 <ampersand8@gmail.com>
2018-01-11 19:55:29 +0100

    add simple search for services table

--- src/main/java/com/swisscom/beans/ServiceBean.java
+++ src/main/java/com/swisscom/beans/ServiceBean.java
@@ -9,7 +9,6 @@ import org.hibernate.query.Query;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.NoResultException;
 import javax.persistence.criteria.CriteriaBuilder;
